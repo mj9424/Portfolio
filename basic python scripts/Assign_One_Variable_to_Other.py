0b39c03d6904e49275f07e70116d20f97fbbd3ff
mj9424 <166155854+mj9424@users.noreply.github.com>
2024-04-30 17:17:08 -0400

    Create Assign_One_Variable_to_Other.py

---
+++ basic python scripts/Assign_One_Variable_to_Other.py
@@ -0,0 +1,13 @@
+# Replace ___ with your code
+
+# create the favorite_food variable
+favorite_food = 'steak'
+
+# print the favorite_food variable 
+print(favorite_food)
+
+# create the food variable
+food = 'pizza'
+
+# assign food to favorite_food
+favorite_food = food
