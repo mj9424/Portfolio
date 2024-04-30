e5640572979a3b7d5f5f52c0e6981d2af6d754d0
mj9424 <166155854+mj9424@users.noreply.github.com>
2024-04-30 16:56:37 -0400

    Create Change_Values_of_Variables.py

---
+++ basic python scripts/Change_Values_of_Variables.py
@@ -0,0 +1,13 @@
+# Replace ___ with your code 
+
+# assign "United States" to the country variable
+country = "United States"
+
+# print the country variable
+print(country)
+
+# assign "Canada" to country
+country = "Canada"
+
+# print the country variable
+print(country)
